8f7d0edce12ca813d5c90038ab1c550a1f1d5407
kushal080bei025-source <kushal.080bei025@tcioe.edu.np>
2026-08-12 12:09:30 +0545

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const authMiddleware = require("./middleware/authenticate");
 const BaseMiddleware = require("./middleware/baseMiddleware");
 const upload = require("./middleware/upload");
 
-const PORT = process.env.PORT || 3000;
 
 const {
   login,
