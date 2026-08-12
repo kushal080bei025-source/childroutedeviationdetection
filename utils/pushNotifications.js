16efba1895e15b82de116275cb153f53bb544796
kushal080bei025-source <kushal.080bei025@tcioe.edu.np>
2026-08-12 10:43:04 +0545

    Update pushNotifications.js

--- utils/pushNotifications.js
+++ utils/pushNotifications.js
@@ -1,4 +1,4 @@
-const admin = require("../config/firebase");
+const admin = require("../firebase");
 const User = require("../db/user.js");
 const Notification = require("../db/notifications.js");
 
