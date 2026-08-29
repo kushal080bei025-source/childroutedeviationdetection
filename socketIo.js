f5acfbf92cc677a25c5f9e33be2dd6aabd1e6403
kusalBhat <bhattakushal2006@gmail.com>
2026-08-29 15:16:49 +0545

    testing added

--- socketIo.js
+++ socketIo.js
@@ -14,14 +14,14 @@ const sendNotification = async (
   try {
     switch (notification_type) {
       case "route_deviated":
-        await sendToUser(user, io, {
+        await sendToUser(req.dbUser, io, {
           type: notification_type,
           title: "Route Deviated",
           body: "Your route has deviated from the planned path.",
         });
         break;
       case "fall_detected":
-        await sendToUser(user, io, {
+        await sendToUser(req.dbUser, io, {
           type: notification_type,
           title: "Fall Detected",
           body: "Your child has fallen.",
